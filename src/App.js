5e16117d20f78f41e7a6aa3e440a263a23b73420
ta93n <nideratakumin@gmail.com>
2020-12-02 15:48:13 +0900

    [add]テスト・コメント追加

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <p>テスト</p>
+    <p className="test">テスト</p> // JSXにクラス名をつける場合「className='クラス名'」とする
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+ReactDOM.render( // ReactDOM.render(<App />, ...と書くことで、App.jsのJSXがHTMLに変換される
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') // ..., document.getElementById('id名'));と書くことで、変換されたHTMLがindex.htmlの指定したid名の要素の中に挿入される
 );
