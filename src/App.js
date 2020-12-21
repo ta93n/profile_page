e584693f7db846eaea0f000188de86ce6684e224
ta93n <nideratakumin@gmail.com>
2020-12-21 22:14:01 +0900

    [remove]footerの削除

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './Components/Header';
 import Main from './Components/Main';
-import Footer from './Components/Footer';
 import './App.scss';
 
 class App extends React.Component {
@@ -10,7 +9,6 @@ class App extends React.Component {
       <div className="app">
         <Header />
         <Main />
-        <Footer />
       </div>
     );
   }
